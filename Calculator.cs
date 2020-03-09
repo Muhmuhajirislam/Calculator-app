65e947cea2f2cc8c8eb7bd71d20ecc44341e3ff4
muh muhajirislam <ajieflamers411@gmail.com>
2020-03-09 17:03:28 +0700

    Tugas 2 fungsi Main

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		  int a = 10;
+          int b = 5;
+		  
+		    Console.WriteLine("Hasil Penambahan" + a + "+" + b + "=" + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}",a,b, Pengurangan(a, b));
+			
+			Console.WriteLine("\nTekan Sembarang Untuk Keluar.");
+            Console.ReadKey();
 	
 	}
 	static int Penambahan(int a, int b)
